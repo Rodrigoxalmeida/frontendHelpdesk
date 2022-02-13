05ed2affb8413af34a78862820edbaeff0ed9a93
Rodrigoxalmeida <rodrigox.almeida@yahoo.com.br>
2022-02-13 19:09:41 -0300

    login

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { Credenciais } from 'src/app/models/credenciais';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null,Validators.email);
+  senha = new FormControl(null,Validators.minLength(3));
+
+  constructor(private toast: ToastrService,
+    private service: AuthService) { }
+
+  ngOnInit(): void {
+  }
+
+  logar(){
+    this.service.authenticate(this.creds).subscribe(resposta => {
+      this.toast.info(resposta.headers.get('Authorization'));
+    },() =>{
+      this.toast.error('Usuário e/ou senha inválidas');
+    })
+  }
+
+  validaCampos(): boolean{
+    return this.email.valid && this.senha.valid; 
+    }
+}
---
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+export class TecnicoListComponent implements OnInit {
+
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Rodrigo de Almeida',
+      cpf: '123.456.789-99',
+      email:'Rodrigo@email.com',
+      senha: '123',
+      perfis:['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
+
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
+
+
